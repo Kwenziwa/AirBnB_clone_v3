2fd388da605522c4f8e088980790fcbc4e1a629d
NosiphoSK1208 <simphiwe95.k@gmail.com>
2023-09-30 21:58:12 +0000

    Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from flask import jsonify
+
+'''Define a route /status on the object app_views 
+ that returns a JSON: "status": "OK"'''
+@app_views.route('/status', methods=['GET'])
+def status():
+    return jsonify({"status": "OK"})
+

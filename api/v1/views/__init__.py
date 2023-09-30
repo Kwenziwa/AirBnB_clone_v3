446a4e3bce9db59971ca1d8ca4b9a393a9fa4471
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-09-30 18:22:58 -0400

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from flask import Blueprint
 
-''' Create a Blueprint instance with a URL prefix of '/api/v1''''
+''' Create a Blueprint instance with a URL prefix of '/api/v1'''
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 '''Wildcard import of everything in the package api.v1.views.index

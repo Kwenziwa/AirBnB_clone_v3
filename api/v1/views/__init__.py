03a510d9a47c97d99285f5597dda23a69df8818d
kwenziwa <kwenziwa@live.com>
2023-10-01 14:00:44 -0400

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+"""
+init file for the app routes with app_views
+"""
 from flask import Blueprint
 
-''' Create a Blueprint instance with a URL prefix of '/api/v1'''
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-'''Wildcard import of everything in the package api.v1.views.index
-  (PEP8 will complain, but it's normal for this file) '''
-from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
-from api.v1.views import app_views
+""" Index module """
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 
-'''Define a route /status on the object app_views 
- that returns a JSON: "status": "OK"'''
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """returns a json"""
     return jsonify({"status": "OK"})
 
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """retrieves the number of each objects by type"""
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+
+    objs = {}
+
+    for key, value in classes.items():
+        count = storage.count(value)
+        objs[key] = count
+    return jsonify(objs)

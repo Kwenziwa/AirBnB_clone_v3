0c3824a6e6b6c3a5caa57e780c309b1cb42c7a3e
kwenziwa <kwenziwa@live.com>
2023-10-01 14:10:27 -0400

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
-""" Script that imports a Blueprint and runs Flask """
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+'''
+Module that defines the first endpoint.
+'''
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
-host = getenv("HBNB_API_HOST", '0.0.0.0')
-port = getenv("HBNB_API_PORT", 5000)
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """closes the current session"""
+def close_storage(exception):
+    '''
+    Method that calls storage.close()
+    '''
     storage.close()
+
+
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """errors that returns a JSON-formatted
-       404 status code response.
-    """
-    return make_response(jsonify({"error": "Not found"}), 404)
+    '''
+    Handler for 404 errors that returns a JSON-formatted 404 response.
+    '''
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(host=host, port=port, threaded=True)
+    app.run(
+        host=getenv('HBNB_API_HOST', '0.0.0.0'),
+        port=int(getenv('HBNB_API_PORT', 5000)),
+        threaded=True
+    )
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-""" Index module """
-from flask import jsonify
+'''
+Module that define the index
+'''
 from api.v1.views import app_views
+from flask import jsonify
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/status')
 def status():
-    """returns a json"""
+    '''
+    Method that return the status of api
+    '''
     return jsonify({"status": "OK"})
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/stats')
 def stats():
-    """retrieves the number of each objects by type"""
-    classes = {
-        "amenities": "Amenity",
-        "cities": "City",
-        "places": "Place",
-        "reviews": "Review",
-        "states": "State",
-        "users": "User"
+    '''
+    Retrieves the number of each object type
+    '''
+    stats_dict = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
-
-    objs = {}
-
-    for key, value in classes.items():
-        count = storage.count(value)
-        objs[key] = count
-    return jsonify(objs)
+    return jsonify(stats_dict)

7fad9a6929b56d73494b06833617e5bd44a5257b
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-10-01 15:17:10 -0400

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,22 @@ from models import storage
 def status():
     """returns a json"""
     return jsonify({"status": "OK"})
+    
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """retrieves the number of each objects by type"""
+    classes = {
+            "amenities": "Amenity",
+            "cities": "City",
+            "places": "Place",
+            "reviews": "Review",
+            "states": "State",
+            "users": "User"
+            }
+
+    objs = {}
+
+    for key, value in classes.items():
+        count = storage.count(value)
+        objs[key] = count
+    return jsonify(objs)

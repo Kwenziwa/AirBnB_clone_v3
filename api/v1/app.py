bbefd52fda869ce56605eac1e31fa4de1c8c8a8d
kwenziwa <kwenziwa@live.com>
2023-10-01 17:38:39 -0400

    buif

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,22 @@
-#!/usr/bin/python3
-""" Script that imports a Blueprint and runs Flask """
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-
-host = getenv("HBNB_API_HOST", '0.0.0.0')
-port = getenv("HBNB_API_PORT", 5000)
+import os
 
 app = Flask(__name__)
+
+# Register the blueprint app_views to your Flask instance app
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-
+# Declare a method to handle @app.teardown_appcontext that calls storage.close()
 @app.teardown_appcontext
-def teardown(exception):
-    """closes the current session"""
+def close_storage(exception):
     storage.close()
 
-
-@app.errorhandler(404)
-def not_found(error):
-    """errors that returns a JSON-formatted
-       404 status code response.
-    """
-    return make_response(jsonify({"error": "Not found"}), 404)
-
-
 if __name__ == "__main__":
+    # Get the host and port from environment variables or use defaults
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    
+    # Run your Flask server (variable app) with the specified host, port, and threaded=True
     app.run(host=host, port=port, threaded=True)
